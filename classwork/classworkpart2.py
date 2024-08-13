d1c184c5ae2f30afcf8a3ac1ef8ace7d8e30a35e
BESIKA77 <besogaming108@gmail.com>
2024-08-13 19:01:31 +0400

    commit

--- classwork/classworkpart2.py
+++
@@ -1,8 +0,0 @@
-
-firstname = input("Please enter your firstname:")
-lastname = input("Please enter your lastname:")
-age = input("Please enter your age:")
-Gmail = input("Please enter your Gmail:")
-
-
-print(firstname + " " +  lastname + " " + age + " " + Gmail)
--- homework/day1/lesson1.py
+++
@@ -1,8 +0,0 @@
-name = "me var beso"
-surname = "gelxauri"
-print(name + " " + surname)
-age = 14
-wlis = "wlis,"
-height =152.5
-print(name + " " + surname)
-print(name + " "+ surname + " " + str(age) + " " + wlis + str(height))
