787c509e071786f427d10e0324dc41c3cfd49828
BESIKA77 <besogaming108@gmail.com>
2024-08-28 23:59:29 +0400

    'commit'

--- level 10/homework/level 10.py
+++ level 10/homework/level 10.py
@@ -7,3 +7,31 @@ for i in range(10):
 
 for i in range(10):
     print('MR.nika is real CHAD')
+
+
+for i in range(10):
+    print('MR.nika is real CHAD')
+
+
+for i in range(10):
+    print('MR.nika is real CHAD')
+
+
+for i in range(10):
+    print('MR.nika is real CHAD')
+
+
+for i in range(10):
+    print('hi')
+
+
+for i in range(10):
+    print('how are u')
+
+
+for i in range(10):
+    print('hi')
+
+
+for i in range(10):
+    print('hello')
