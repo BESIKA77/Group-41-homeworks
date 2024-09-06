6ae8f4b3c451c6b059809b14a43970305a982f4f
BESIKA77 <besogaming108@gmail.com>
2024-09-06 16:34:41 +0400

    'commit'

---
+++ day 01/level 01.py
@@ -0,0 +1,6 @@
+name = 'beso'
+surname = 'gelxauri'
+age = 15
+height = 1.53
+
+print("me var" " " + name + "," + " " "chemi gvaria" " " + surname + "." + " " + "me var " + " " + str(age) + " " + "wlis." + " " + "me var " + " " + str(height) + "cm simaglis." )
