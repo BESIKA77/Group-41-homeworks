f3d7feeb0dd54b52e23ea1970c002d0b17fdf12d
BESIKA77 <besogaming108@gmail.com>
2024-09-14 00:46:54 +0400

    commit

--- day 01/level 01.py
+++ day 01/level 01.py
@@ -4,3 +4,15 @@ age = 15
 height = 1.53
 
 print("me var" " " + name + "," + " " "chemi gvaria" " " + surname + "." + " " + "me var " + " " + str(age) + " " + "wlis." + " " + "me var " + " " + str(height) + "cm simaglis." )
+
+
+
+# while True:
+#     number = 87
+#     guess = int(input('guess the number!: '))
+
+#     while guess != number:
+#         guess = int(input('incorrect, guess the number again!: '))
+
+#     if guess == number:
+#         print('congrats!')
